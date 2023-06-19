f76e3ffa9f8a1b79ed1fc3341666b8159998a8ca
KhUts0M <khutsokwena.madiba@gmail.com>
2023-06-19 20:33:58 +0200

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+}
+let apiKey = "b86b691495b465806abd366b749605e1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Polokwane&appid=${apiKey}&units=metric`;
 
+axios.get(apiUrl).then(displayTemperature);
