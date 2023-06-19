f28d1c5605df8a889190ca2d17e215c453b29c15
KhUts0M <khutsokwena.madiba@gmail.com>
2023-06-19 22:13:25 +0200

    updated the file contents more on app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,40 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 0) {
+    hours`0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 0) {
+    minutes`0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "b86b691495b465806abd366b749605e1";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Polokwane&appid=${apiKey}&units=metric`;
